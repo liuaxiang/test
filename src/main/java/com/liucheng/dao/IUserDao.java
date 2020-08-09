debf5dff622632ec21a15521a5f1106c8bf3b0d8
liucheng <1549803078@qq.com>
2020-08-09 17:59:37 +0800

    提交test

---
+++ src/main/java/com/liucheng/dao/IUserDao.java
@@ -0,0 +1,19 @@
+package com.liucheng.dao;
+
+
+import com.liucheng.domin.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+
+public interface IUserDao {
+
+    /**
+     * 查询所有操作
+     * @return
+     */
+    @Select("select * from user")
+    List<User> findAll();
+
+}
---
+++ src/main/java/com/liucheng/dao/Test.java
@@ -0,0 +1,12 @@
+package com.liucheng.dao;
+
+import com.liucheng.domin.User;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+    }
+}
